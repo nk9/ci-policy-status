d915c4ab2f2046364653a033fb4c578898a1d09c
Nick Kocharhook <nick@kocharhook.com>
2023-07-08 12:48:30 +0100

    Move from home grown disclosure triangles to MUI accordion components.

--- components/CIAsk.js
+++ components/CIAsk.js
@@ -8,59 +8,22 @@ import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 
 import {
-    IconButton,
+    Accordion, AccordionSummary, AccordionDetails, IconButton, Typography
 } from "@mui/material";
 
 export default function ({ target_name, targets, image, body }) {
-    const content_id = slugify(target_name)
-    const [expanded, setExpanded] = React.useState(false)
-
-    const openQuickView = (toggle, fullwidth) => {
-        toggle.setAttribute('aria-expanded', 'true');
-        fullwidth.classList.toggle(styles.is_hidden);
-        // Make fullwidth card keyboard focusable.
-        fullwidth.setAttribute('tabIndex', '0');
-    };
-
-    const closeQuickView = (toggle, fullwidth) => {
-        toggle.setAttribute('aria-expanded', 'false');
-        fullwidth.classList.toggle(styles.is_hidden);
-        fullwidth.removeAttribute('tabIndex');
-    };
-
-    var clickOpenQuickView = (e) => {
-        // debugger;
-        const toggle = e.target.closest("button")
-        const content_id = toggle.dataset.contentId
-        const content = document.getElementById(content_id)
-
-        if (toggle.getAttribute('aria-expanded') !== 'false') {
-            openQuickView(toggle, content)
-        } else {
-            closeQuickView(toggle, content)
-        }
-        setExpanded(!expanded)
-    }
-
     return (
-        <>
-            <li className={styles.target_name} key={content_id}>
-                <div className={styles.d_flex}>
-                    <IconButton
-                        size="small"
-                        onClick={clickOpenQuickView}
-                        data-content-id={content_id}
-                        sx={{ height: 'max-content' }}>
-                        {expanded ? <CloseIcon /> : <AddIcon />}
-                    </IconButton>
-                    <span style={{ fontSize: "16pt" }}>{target_name}</span>
-                </div>
-            </li>
-            <li className={clsx(styles.fullwidth, styles.is_hidden)} id={content_id} key={content_id + "_body"} >
+        <Accordion>
+            <AccordionSummary>
+                <Typography sx={{ flex: 1 }}>{target_name}</Typography>
+                {React.Children.toArray(targets.map((t) => <Typography sx={{ width: '45px', flexShrink: 0, textAlign: 'center' }}>{t}</Typography>))}
+            </AccordionSummary>
+            <AccordionDetails>
                 {image ?? null}
                 {body}
-            </li>
-            {React.Children.toArray(targets.map((t) => <li><p>{t}</p></li>))}
-        </>
+            </AccordionDetails>
+            
+        </Accordion>
     )
 }
+
