08d7c5ecf3bd1c193cfa41b70f867b974df515f8
Nick Kocharhook <nick@kocharhook.com>
2023-07-20 21:24:56 +0100

    Align fonts and colors with the rest of the CI site.

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,12 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
+let linkColor = '#919191'
+let linkHoverColor = '#386FA7'
+
 // Create a theme instance.
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
     error: {
       main: red.A400,
     },
@@ -17,22 +14,45 @@ const theme = createTheme({
       main: '#000'
     }
   },
+  typography: {
+    fontFamily: '"Ubuntu", sans-serif',
+    fontWeight: 500
+  },
+  components: {
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: linkColor,
+          textDecorationColor: linkColor,
+          fontWeight: 500,
+          ":hover": {
+            color: linkHoverColor,
+            textDecorationColor: linkHoverColor,
+            fontWeight: 500,
+          }
+        },
+      }
+    }
+  }
 });
 
 theme.typography.h3 = {
   fontSize: '1.5rem',
+  fontFamily: "'Playfair Display', serif",
   [theme.breakpoints.up('md')]: {
     fontSize: '2.4rem',
   },
 }
 theme.typography.h4 = {
   fontSize: '1.3rem',
+  fontFamily: "'Playfair Display', serif",
   [theme.breakpoints.up('md')]: {
     fontSize: '2rem',
   },
 }
 theme.typography.h5 = {
   fontSize: '1.1rem',
+  fontFamily: "'Playfair Display', serif",
   [theme.breakpoints.up('md')]: {
     fontSize: '1.4rem',
   },
